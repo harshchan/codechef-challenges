f5e559084cbe88984548af509aec9793d49e6a33
Harsh Chandekar <59416466+harshchan@users.noreply.github.com>
2021-02-05 23:02:09 +0530

    Create HDIVISR.cpp

---
+++ February Challenge 2021/HDIVISR.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;int x=10,t;
+	cin>>n;
+	while(n%x!=0)
+	{
+	   
+	    x--;
+	}
+	cout<<x;
+	return 0;
+}
